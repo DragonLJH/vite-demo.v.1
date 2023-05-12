6a372237665d5d00466441293270d69c7877d172
DragonLJH <1183665002@qq.com>
2023-05-12 17:46:42 +0800

    重写WebMvcConfigurer（全局跨域）

---
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    // 重写WebMvcConfigurer（全局跨域）
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("*")
+                        .allowCredentials(true)
+                        .maxAge(3600)
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
